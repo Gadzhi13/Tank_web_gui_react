ebbee1a84579406d6ba98f907965c2f707e8f76e
Gadzhi <abdurazakov.gadzhi@gmail.com>
2021-03-07 01:20:07 +0100

    first logic spotify connect

--- src/components/Spotify/Spotify.tsx
+++ src/components/Spotify/Spotify.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from 'react'
 import { Form, FormControl, FormLabel,  Button, FormGroup } from 'react-bootstrap'
 
+import { useSpotifyConnect } from '../../util/spotify/spotifyController'
+
 //TODO: add alphanumeric check on cleint ID
 
 const Spotify = () => {
 
     const [clientId, setClientId] = useState('')
+    const [clientPwd, setClientPwd] = useState('')
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const useHandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         let clientIdBuffer = event.currentTarget.formControlId.value
+        let clientPwdBuffer = event.currentTarget.formControlPassword.value
         setClientId(clientIdBuffer)
-        console.log(clientId)
+        setClientPwd(clientPwdBuffer)
+        useSpotifyConnect(clientId, clientPwd)
     }
 
     return (
         <div>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={useHandleSubmit}>
                 <FormGroup controlId="formControlId">
                     <FormLabel>Control Id:</FormLabel>
-                    <FormControl size="lg" type="clientId" placeholder="Enter client ID"></FormControl>
-                    <Button size="lg" variant="dark" type="submit">Submit</Button>
+                    <FormControl size="lg" type="text" placeholder="Enter client ID"></FormControl>
                 </FormGroup>
+                <FormGroup controlId="formControlPassword">
+                    <FormControl size="lg" type="password" placeholder="Enter client Password"></FormControl>
+                </FormGroup>
+                    <Button size="lg" variant="dark" type="submit">Submit</Button>
             </Form>
         </div>
     )
--- src/util/spotify/spotifyController.tsx
+++ src/util/spotify/spotifyController.tsx
@@ -1,3 +1,21 @@
-export const useSpotifyConnect = () => {
+import axios from 'axios'
 
+export const useSpotifyConnect = (clientId: String, clientPassword: String) => {
+    let clientString: string = clientId + ':' + clientPassword
+    axios({
+        method: 'POST',
+        url: 'https://accounts.spotify.com/api/token',
+        data: 'grant_type=client_credentials',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(clientString).toString('base64')
+        }
+    })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log("err caught - ")
+            console.log(err)
+        })
 }
