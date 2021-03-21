b312baf1f68dd80e60857323aede83d1f1fadc72
Gadzhi <abdurazakov.gadzhi@gmail.com>
2021-03-21 23:14:44 +0100

    more accordion fixes

--- src/components/Spotify/Player/Playlists/Playlists.tsx
+++ src/components/Spotify/Player/Playlists/Playlists.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Accordion, Button, ListGroup } from 'react-bootstrap'
+import { Accordion, Card, useAccordionToggle } from 'react-bootstrap'
 
 import { spotifyRequestHandler } from '../../../../util/spotify/spotifyController'
 import { PlaylistsProps, PagingObject, SimplifiedPlaylist } from '../../../../types/Spotify'
@@ -7,9 +7,9 @@ import Playlist from './Playlist/Playlist'
 
 const Playlists = (props: PlaylistsProps) => {
     const [playlists, setPlaylists] = useState<SimplifiedPlaylist[]>()
-    const [showPlaylists, setShowPlaylists] = useState<boolean>(false)
 
     const getPlaylists = (): void => {
+        if (playlists || !props.accessToken) return
         spotifyRequestHandler(props.accessToken, '/me/playlists', 'GET')
             .then((res: PagingObject<SimplifiedPlaylist>) => {
                 try {
@@ -21,22 +21,24 @@ const Playlists = (props: PlaylistsProps) => {
     }
 
     const togglePlaylists = () => {
-        setShowPlaylists(!showPlaylists)
-        if (playlists || !props.accessToken) return
         getPlaylists()
     }
 
     return (
-        <div>
-            <Button variant='dark' onClick={togglePlaylists}>Toggle Playlists</Button>
-            {showPlaylists &&
-                <Accordion>
-                    {playlists ? playlists.map((el, index) => {
-                        return (<Playlist accessToken={props.accessToken} playlist={el} index={index}></Playlist>)
-                    }) : null}
-                </Accordion>
-            }
-        </div>
+        <Accordion>
+            <Card>
+                <Accordion.Toggle as={Card.Header} onClick={togglePlaylists} eventKey='200'>
+                        Toggle Playlists
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey='200'>
+                    <Accordion>
+                        {playlists ? playlists.map((el, index) => {
+                            return (<Playlist accessToken={props.accessToken} playlist={el} index={index} key={index}></Playlist>)
+                        }) : null}
+                    </Accordion>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
     )
 }
 
